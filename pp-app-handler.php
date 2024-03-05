fb241c950100418268d930d916445f1f26871db3
alordiel <alexander.vasilev@protonmail.com>
2024-03-05 15:49:46 +0200

    initial file

---
+++ pp-app-handler.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: WordPress - App Bridge for Practice Portuguese
+ * Plugin URI:
+ * Description: Handles things like login session and etc.
+ * Author: Alexander Vasilev
+ * Author URI:
+ * Version: 0.1
+ * Text Domain: pp-app-bridge
+ */
+
+defined( 'ABSPATH' ) or exit;
+
